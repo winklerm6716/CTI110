404fb2fb0d26a3379edb2de98620bff40132da3e
winklerm6716 <winklerm6716@student.faytechcc.edu>
2017-06-06 18:06:50 -0400

    Add files via upload
    Distance Traveled

---
+++ M2HW1_DistanceTraveled_Winkler.py
@@ -0,0 +1,13 @@
+# Distance Traveled
+# 6/6/17
+# CTI110 - M2HW1 - Distance Traveled
+# Michael Winkler
+#
+speed = 70
+print('In 6 hours, the car will travel ' + str((speed * 6)) + ' miles.')
+print('In 10 hours, the car will travel ' + str((speed * 10)) + ' miles.')
+print('In 15 hours, the car will travel ' + str((speed * 15)) + ' miles.')
+
+
+
+
