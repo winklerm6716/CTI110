99637bfa150bfe4e7c1e0589a919a01fa63b69b0
winklerm6716 <winklerm6716@student.faytechcc.edu>
2017-06-06 17:11:37 -0400

    Add files via upload
    Module 2 Tutorial Assignment

---
+++ M2T1_SalesPerdiction_MichaelWinkler.py
@@ -0,0 +1,8 @@
+# Getting Projected Total Sales
+# 6/6/17
+# CTI-110 M2T1 - Sales Prediction
+# Michael Winkler
+#
+total_sales = float(input('Enter the projected sales: '))
+profit = total_sales * 0.23
+print('The profit is $', profit)
