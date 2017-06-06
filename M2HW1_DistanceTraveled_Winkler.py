85ad7c865aa26d5c5f0ea8e201ac2c592999366e
winklerm6716 <winklerm6716@student.faytechcc.edu>
2017-06-06 18:05:38 -0400

    Delete M2HW1_DistanceTraveled_Winkler.py

--- M2HW1_DistanceTraveled_Winkler.py
+++
@@ -1,9 +0,0 @@
-# Distance Traveled Calculation
-# 6/6/17
-# CTI-110 M2HW1 - Distance Traveled
-# Michael Winkler
-#
-land_calculation = float(input('Enter the total square feet: '))
-acres = land_calculation / 43560
-print('The acres in the tract are', acres)
-
