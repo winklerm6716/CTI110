66a39ffd8030b210673a5ee1ef8ce88e0c04c11a
winklerm6716 <winklerm6716@student.faytechcc.edu>
2017-06-13 14:54:28 -0400

    Add files via upload
    Debugging Exercise

---
+++ M3T2_Debugging_MichaelWinkler.py
@@ -0,0 +1,20 @@
+# Debugging Tutorial
+# 6/13/17
+# CTI-110 M3T2 - Debugging
+# Michael Winkler
+#
+
+if score >= A_score:
+    print('Your grade is A.')
+else:
+    if score >= B_score:
+        print('Your grade is B.')
+    else:
+        if score >= C_score:
+            print('Your grade is C.')
+        else:
+            if score >= D_score:
+                print('Your grade is D.')
+            else:
+                print('Your grade is F.')
+
